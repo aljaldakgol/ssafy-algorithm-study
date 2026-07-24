68f49bbc6c85cccff059ae938c3ae6ad43fb858a
Tujungmin <paulkjm92@gmail.com>
2026-07-24 17:52:31 +0900

    chore: SWEA 1974, 5215 풀이용 파일 생성

---
+++ week01/곽정민/SWEA_1974_스도쿠_검증.java
@@ -0,0 +1,22 @@
+/*
+ * 문제: SWEA 1974 스도쿠 검증
+ * 알고리즘:
+ */
+
+import java.util.Scanner;
+import java.io.IOException;
+
+public class SWEA_1974_스도쿠_검증 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+
+        }
+
+    }
+}
---
+++ week01/곽정민/SWEA_5215_햄버거_다이어트.java
@@ -0,0 +1,22 @@
+/*
+ * 문제: SWEA 5215 햄버거 다이어트
+ * 알고리즘:
+ */
+
+import java.util.Scanner;
+import java.io.IOException;
+
+public class SWEA_5215_햄버거_다이어트 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+
+        }
+
+    }
+}
