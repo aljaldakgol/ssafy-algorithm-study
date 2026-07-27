afb91999c8af83feb9f204851cb825d36ddf7f37
문하늘 <melody0404@naver.com>
2026-07-28 02:27:02 +0900

    solve: SWEA 1974 스도쿠 검증 문제 풀이

---
+++ week01/SWEA_1974_스도쿠_검증.java
@@ -0,0 +1,70 @@
+package week01;
+
+import java.util.Scanner;
+//import java.io.FileInputStream;
+
+
+public class SWEA_1974_스도쿠_검증 {
+	public static void main(String args[]) throws Exception {
+	
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+	
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			int[][]matrix = new int[9][9];
+			for (int i = 0; i<9; i++) {
+				for(int j = 0; j<9; j++) {
+					matrix[i][j]=sc.nextInt();
+				}
+			}
+			
+			int result = 1;
+			
+			
+			// 1) row
+			for (int i = 0; i<9; i++) {
+				for (int j = 0 ; j<8; j++) {
+					for(int k = 1; k<9-j; k++) {
+						if(matrix[i][j]==matrix[i][j+k]) {
+							result = 0;
+						}
+					}
+				}
+			}
+			// 2) column
+			for (int i = 0; i<9; i++) {
+				for (int j = 0 ; j<8; j++) {
+					for(int k = 1; k<9-j; k++) {
+						if(matrix[j][i]==matrix[j+k][i]) {
+							result= 0;
+						}
+					}
+				}
+			}
+			// 3) small
+			for (int i = 0; i < 9; i = i + 3) {
+			    for (int j = 0; j < 9; j = j + 3) {
+
+			        boolean[] check = new boolean[10]; 
+			        
+			        for (int k = 0; k < 3; k++) {
+			            for (int l = 0; l < 3; l++) {
+			                int num = matrix[i + k][j + l];
+			                
+			                if (check[num] == true) {
+			                    result = 0; 
+			                } else {
+			                    check[num] = true; 
+			                }
+			            }
+			        }
+			        
+			    }
+			}
+				
+			System.out.printf("#%d %d\n", test_case,result);
+		}
+	}
+}
