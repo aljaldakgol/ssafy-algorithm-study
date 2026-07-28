9a1fc6c142f8c4f34ae2e38ad19071605f14d063
Tujungmin <paulkjm92@gmail.com>
2026-07-28 16:53:31 +0900

    chore: SWEA 소수찾기 코드 포맷팅 정리

--- week01/곽정민/Solution_소수찾기_곽정민.java
+++ week01/곽정민/Solution_소수찾기_곽정민.java
@@ -4,24 +4,23 @@
  */
 
 public class Solution_소수찾기_곽정민 {
-    public static int solution(int n) {
-        
-  boolean[] check = new boolean[n + 1];
-	check[0] = check[1] = true;
-    int answer = 0;
-	for (int i = 2; i * i <= n; i++) {
-		if (check[i]) continue;
-		for (int j = i + i; j <= n; j += i)
-			check[j] = true;
-	}
-    
-	for (int i = 2; i <= n; i++)
-		if (!check[i]) {
-            answer++;
-        }
-        
-        return answer;
-    }
-}
+	public static int solution(int n) {
 
-	
+		boolean[] check = new boolean[n + 1];
+		check[0] = check[1] = true;
+		int answer = 0;
+		for (int i = 2; i * i <= n; i++) {
+			if (check[i])
+				continue;
+			for (int j = i + i; j <= n; j += i)
+				check[j] = true;
+		}
+
+		for (int i = 2; i <= n; i++)
+			if (!check[i]) {
+				answer++;
+			}
+
+		return answer;
+	}
+}
