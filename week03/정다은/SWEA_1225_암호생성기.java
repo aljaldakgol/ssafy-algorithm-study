d6fde6b89c07f68c31f8b618e045ccead540f9ed
정다은 <kate030551@naver.com>
2026-08-07 09:25:32 +0900

    SWEA_1225_암호생성기

---
+++ week03/정다은/SWEA_1225_암호생성기.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.util.LinkedList;
+import java.util.Queue;
+
+// 선입선출이라 큐 사용
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		for(int test_case = 1; test_case <= 10; test_case++)
+		{
+            int t = sc.nextInt();
+			Queue<Integer> queue = new LinkedList<>();
+            for (int i=0;i<8;i++) {
+            	queue.add(sc.nextInt());
+            }
+            
+            int num;
+            boolean b = true;
+            // 0이 나올 때까지 while
+            while (b) {
+            	// 한 사이클은 5번 반복
+                for (int i=1;i<6;i++) {
+                    num = queue.poll();
+                    num -= i;
+                    
+                    if (num <= 0 ) {
+                    	num = 0;
+                    }
+                	queue.add(num);
+                    if (num == 0) {
+                        b = false;
+                    	break;
+                    }
+                }
+            }
+            
+            System.out.print("#"+test_case+" ");
+            for (int i=0;i<8;i++) {
+            	System.out.print(queue.poll()+" ");
+            }
+            System.out.println();
+		}
+	}
+}
