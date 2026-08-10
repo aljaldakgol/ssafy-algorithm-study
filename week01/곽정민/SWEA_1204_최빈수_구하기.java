04e76e8003fc539689838eaa0743fae8fb660c40
Tujungmin <paulkjm92@gmail.com>
2026-08-10 09:37:04 +0900

    chore 1주차 내용 충돌 해결

--- week01/곽정민/SWEA_1204_최빈수_구하기.java
+++ week01/곽정민/SWEA_1204_최빈수_구하기.java
@@ -23,10 +23,10 @@ public class SWEA_1204_최빈수_구하기 {
 
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		    StringTokenizer st;
+            //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		    //StringTokenizer st;
 
-		int T = Integer.parseInt(br.readLine());
+		    //int T1 = Integer.parseInt(br.readLine());
             int casenum = sc.nextInt();
 
             // score_count[n] = 값 n이 등장한 횟수 (0~100 범위이므로 크기 101)
