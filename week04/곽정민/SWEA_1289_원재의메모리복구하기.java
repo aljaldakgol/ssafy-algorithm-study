2823a3fd02fb05afc137ac13356a674a77d27cdc
Tujungmin <paulkjm92@gmail.com>
2026-08-20 17:54:25 +0900

    chore:파일생성

---
+++ week04/곽정민/SWEA_1289_원재의메모리복구하기.java
@@ -0,0 +1,31 @@
+package week04.곽정민;
+import java.io.*;
+import java.util.*;
+
+
+public class SWEA_1289_원재의메모리복구하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int test_case = Integer.parseInt(br.readLine());
+        for(int i = 0; i<test_case; i++) {
+           
+          String bits = br.readLine();
+          boolean toggle = false;
+          int count = 0;
+          for(int ch = 0; ch<bits.length(); ch++) {
+            
+
+          }
+
+          }
+
+
+        }
+      
+
+
+          
+        
+    }
+}
