fda3dfaac4b213260245197afa631e243e645daf
Tujungmin <paulkjm92@gmail.com>
2026-08-24 17:54:33 +0900

    chore: 코드 작성. 아직 미완성

---
+++ week05/곽정민/SWEA_3499_퍼펙트셔플.java
@@ -0,0 +1,42 @@
+package week05.곽정민;
+
+import java.util.*;
+import java.io.*;
+public class SWEA_3499_퍼펙트셔플   {
+
+  static String[] perfectShuffle(String[] cards, int n) {
+    String[] result = new String[n];
+
+    // TODO: 절반으로 나누고 교대로 뽑아서 result 채우기
+    // - n이 홀수면 앞쪽(front)에 한 장 더 들어가게
+
+    return result;
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    int test_case = Integer.parseInt(br.readLine().trim());
+
+    for (int tc = 1; tc <= test_case; tc++) {
+      int n = Integer.parseInt(br.readLine().trim());
+
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      String[] cards = new String[n];
+      for (int i = 0; i < n; i++) {
+        cards[i] = st.nextToken();
+      }
+
+      String[] shuffled = perfectShuffle(cards, n);
+
+      sb.append("#").append(tc);
+      for (String card : shuffled) {
+        sb.append(" ").append(card);
+      }
+      sb.append("\n");
+    }
+
+    System.out.print(sb);
+  }
+}
