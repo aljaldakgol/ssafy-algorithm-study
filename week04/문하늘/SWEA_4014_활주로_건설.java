86cb7e819e1de80413f63c14b05f7136ba4f927c
moonaneul <melody0404@naver.com>
2026-08-26 17:38:11 +0900

    solve : SWEA_4014_활주로_건설.java try n pass!!

--- week04/문하늘/SWEA_4014_활주로_건설.java
+++ week04/문하늘/SWEA_4014_활주로_건설.java
@@ -18,7 +18,6 @@ public class SWEA_4014_활주로_건설 {
 			int slopeLen = Integer.parseInt(st.nextToken());
 			int slopeCount=0;
 			boolean isSlope;
-			boolean isAllSame;
 			
 			int [][] map = new int[mapSize][mapSize];
 			
@@ -31,29 +30,29 @@ public class SWEA_4014_활주로_건설 {
 				}
 			}
 			
-			// 1) 기본적으로 활주로를 만들 수 있음 - 만들 수 없는 경우는 ? 1. 한 번에 2칸이상 줄어든다 2. 한 칸 줄기는 했는데 줄어 든 후 그 수가 반복되는 게 길이보다 작다. 
-			// 줄어들거나 늘거나 인데 그럼 차이를 절댓값으로 쓰면 되겠다. 
-			// false 가 나오면 그 줄은 더 검사할 거 없이 넘어간다. 
+			// 1) 기본적으로 활주로를 만들 수 있음 - 만들 수 없는 경우는 ? 1. 한 번에 2칸이상 줄어든다 2. 한 칸 줄기는 했는데 줄어 든 후 그 수가 반복되는 게 길이보다 작다. 
+			// 줄어들거나 늘거나 인데 그럼 차이를 절댓값으로 쓰면 되겠다. 
+			// false 가 나오면 그 줄은 더 검사할 거 없이 넘어간다. 
 			/**
 			 * 생각 정리..
 			 * 높이가 줄어들면 줄어든 상태로 x번 이상 계속 그 상태 유지 해야 하고
-			 * 높이가 늘어나면 늘어나기 전 상태로 x번 이상 그 상태를 유지하고 있었어야 한다. 
-			 * 그 높이는 한 칸 이상 늘어나서는  안된다. 
-			 * 
+			 * 높이가 늘어나면 늘어나기 전 상태로 x번 이상 그 상태를 유지하고 있었어야 한다. 
+			 * 그 높이는 한 칸 이상 늘어나서는  안된다. 
+			 * 
 			 * 즉 높이가 두칸 이상 벌어지거나
 			 * 높이가 한 칸 늘어났는데 그 전에 x번 이상 반복되지 않았거나
 			 * 높이가 한 칸 줄어들었는데 그 상태 값이 x번 이상 반복되지 않는다면
 			 * 경사로가 만들어지지 않는다.
-			 * 
+			 * 
 			 * 예외 케이스를 처리하는 게 좋은가 적합한 케이스를 세는 게 좋을까
-			 * 
-			 * 또한 절댓값으로는 못 할 거 같다. 두 경우에 처리하는 법이 달라 보인다. 
-			 * 
+			 * 
+			 * 또한 절댓값으로는 못 할 거 같다. 두 경우에 처리하는 법이 달라 보인다. 
+			 * 
 			 * 1) 두 칸 이상 차이 나면 더 볼 거 없다
 			 * 2) 한 칸 줄어든다 -> 그 다음부터 몇 번 그게 반복되는지 센다 근데 ... 그럼 그 지점에서부터 다시 for문을 돌아야 한다는 건가?
-			 * 3) 한 칸 늘어난다 -> 그 전에 그게 몇 번 반복되었는지 센다.... 그럼 그 지점부터반대 방향으로 for문??/? 
+			 * 3) 한 칸 늘어난다 -> 그 전에 그게 몇 번 반복되었는지 센다.... 그럼 그 지점부터반대 방향으로 for문??/? 
 			 * 일단 go 단, map의 범위를 넘어서지 않아야 하는데....
-			 * 
+			 * 
 			 */
 			// 행 별, 열 별로 다 봐야 햔다.
 
@@ -65,11 +64,10 @@ public class SWEA_4014_활주로_건설 {
 				boolean [] slopeVisited = new boolean[mapSize];
 
 				isSlope = true;
-				isAllSame = true;
 				
 				for (int l = 0; l<mapSize-1; l++) {
 					//1) 두 칸 이상 차이 -> 경사로 불가능 볼거 없음
-					if(Math.abs(map[k][l]-map[k][l+1])>1) {isAllSame=false; isSlope = false; break;}
+					if(Math.abs(map[k][l]-map[k][l+1])>1) {isSlope = false; break;}
 					
 					// 2) 한 칸 증 또는 감
 					int xCount;	// 증 또는 감소한 상태로 얼마나 지속?
@@ -77,7 +75,6 @@ public class SWEA_4014_활주로_건설 {
 					
 					// 2-1) 한 칸 증가
 					if(map[k][l+1]-map[k][l]==1) {
-						isAllSame=false;
 						check = 1;		// 뒤쪽으로 방문한다 -얼마나 뒤로 갈 수 있는지 체크
 						xCount = 1; 		// 일단 감소했던 게 하나 있고 지금까지 얼마나 그 상태가 지속 되었는가
 						slopeVisited[l]= true;
@@ -92,9 +89,9 @@ public class SWEA_4014_활주로_건설 {
 					
 					// 2-2) 한 칸 감소
 					if(map[k][l]-map[k][l+1]==1) {
-						isAllSame=false;
-						check = 1;
+ 						check = 1;
 						xCount = 1;
+						slopeVisited[l+check]= true;
 						while((l+1+check)< mapSize) {
 							if(map[k][l+1+check] == map[k][l+1]) { slopeVisited[l+1+check]= true; xCount++; check++;}
 							else {break;}
@@ -103,10 +100,10 @@ public class SWEA_4014_활주로_건설 {
 						}
 						
 						if(xCount<slopeLen) {
-							for (int i = l; i<l+1+check; i++) {
-								slopeVisited[i]= false; 
+							for (int i = l+1; i<l+1+check; i++) {
+								slopeVisited[i]= false;
 							}
-							isSlope=false; 
+							isSlope=false;
 							break;
 							
 						} // 길이짧네 경사로 불가능
@@ -116,7 +113,7 @@ public class SWEA_4014_활주로_건설 {
 //					
 				}
 				
-				if(isSlope||isAllSame) {slopeCount++;}
+				if(isSlope) {slopeCount++;}
 				
 			}
 			
@@ -125,11 +122,10 @@ public class SWEA_4014_활주로_건설 {
 				boolean [] slopeVisited = new boolean[mapSize];
 
 				isSlope = true;
-				isAllSame = true;
 
 				for (int l = 0; l<mapSize-1; l++) {
 					//1) 두 칸 이상 차이
-					if(Math.abs(map[l][k]-map[l+1][k])>1) {isAllSame=false; isSlope = false; break;}
+					if(Math.abs(map[l][k]-map[l+1][k])>1) {isSlope = false; break;}
 					
 					// 2) 한 칸 증 또는 감
 					int xCount;	// 증 또는 감소한 상태로 얼마나 지속?
@@ -137,8 +133,7 @@ public class SWEA_4014_활주로_건설 {
 					
 					// 2-1) 한 칸 증가
 					if(map[l+1][k]-map[l][k]==1) {
-						isAllSame=false;
-						check = 1;		// 뒤쪽으로 방문한다
+ 						check = 1;		// 뒤쪽으로 방문한다
 						xCount = 1; 		// 일단 감소했던 게 하나 있고 지금까지 얼마나 그 상태가 지속 되었는가
 						slopeVisited[l]= true;
 
@@ -152,18 +147,19 @@ public class SWEA_4014_활주로_건설 {
 					
 					// 2-2) 한 칸 감소
 					if(map[l][k]-map[l+1][k]==1) {
-						isAllSame=false;
-						check = 1;
+ 						check = 1;
 						xCount = 1;
+						slopeVisited[l+check]= true;
 						while((l+1+check)<mapSize) {
 							if(map[l+1+check][k]==map[l+1][k]) {slopeVisited[l+1+check]= true; xCount++; check++;}
 							else {break;}
+							
 							if(xCount==slopeLen) {break;}
 
 						}
 						if(xCount<slopeLen) {
-							for (int i = l; i<l+1+check; i++) {
-								slopeVisited[i]= false; 
+							for (int i = l+1; i<l+1+check; i++) {
+								slopeVisited[i]= false;
 							}
 							isSlope=false;
 							break;
@@ -171,7 +167,7 @@ public class SWEA_4014_활주로_건설 {
 					}					
 				}
 				
-				if(isSlope||isAllSame) {slopeCount++;}
+				if(isSlope) {slopeCount++;}
 				
 			}
 			
